1a9080e88cf99de476cd6fad36e27c526d4c656d
D <duytk.job@gmail.com>
2025-02-26 07:24:28 -0500

    update get voices

--- get_voices.py
+++ get_voices.py
@@ -5,10 +5,7 @@ import asyncio
 async def list_voices():
     voices = await edge_tts.list_voices()
     for voice in voices:
-        # print(f"Name: {voice.get('ShortName', 'N/A')}")
-        # print(f"  Gender: {voice.get('Gender', 'N/A')}")
-        # print(f"  Locale: {voice.get('Locale', 'N/A')}")
-        print({'country': voice.get('Locale', 'N/A'),
+        print({'country': voice.get('FriendlyName', 'N/A'),
                'gender': voice.get('Gender', 'N/A'),
                'code': voice.get('ShortName', 'N/A')})
 
