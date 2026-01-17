3a64d59abc978e3b30c7965d5395b47139401024
D <duytk.job@gmail.com>
2026-01-17 00:57:26 -0500

    update auto clicker

--- auto_clicker.py
+++ auto_clicker.py
@@ -12,7 +12,7 @@ import threading
 import random
 
 # Configuration - customize these as needed
-TRIGGER_KEY = 'r'  # Keyboard key that triggers the actions
+TRIGGER_KEY = 'f'  # Keyboard key that triggers the actions
 
 # Keys to press in sequence (set to [] for none)
 KEYS_TO_PRESS = ['t', 'l']  # Press R then F
